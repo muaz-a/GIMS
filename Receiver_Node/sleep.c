7e4ddc1cb75d072dc4bb564a9087f24cb91f0a91
irwincchan <58605004+irwincchan@users.noreply.github.com>
2020-03-31 07:04:40 -0600

    Clear global backToSleep before WFI

--- Receiver_Node/sleep.c
+++ Receiver_Node/sleep.c
@@ -2,13 +2,14 @@
 
 volatile uint8_t alarm = 0;
 
-void sleep(uint32_t seconds){
+void sleep(uint32_t seconds)
+{
     /*int dacOn = 0;
     int adcOn = 0;
-    
+
     //  Checks for ADC, DAC, disables
     //  Remember which ones to re-enable
-    
+
     if ((DAC->CR & DAC_CR_EN1)==DAC_CR_EN1){
         DAC->CR &= ~DAC_CR_EN1;
         dacOn++;
@@ -25,43 +26,43 @@ void sleep(uint32_t seconds){
         ADC2->CR2 &= ~ADC_CR2_ADON;
         adcOn = adcOn+2;
     }*/
-    
+
     // Setup STOP mode
     SCB->SCR |= SCB_SCR_SLEEPDEEP;
     PWR->CR &= ~PWR_CR_PDDS; // set to stop mode
     PWR->CR |= PWR_CR_LPDS; // set low power mode
     PWR->CR |= PWR_CR_CWUF; // clear WUF
-    
-		// Send Xbee to sleep
-		XbeeSleep();
-		
+
+    // Send Xbee to sleep
+    XbeeSleep();
+
     //Enable exit conditions, STOP
     //EXTI8Init(); //Enable to allow early exit
     Configure_RTC(seconds);
     alarm = 0;
-		
-		__WFI();
-		
-		while (!alarm)
-		{			
-			__WFI();
-			backToSleep = 0;
-		}		
-    
-		// Wake Xbee up
-		XbeeWake();
+    backToSleep = 0;
+    __WFI();
+
+    while(!alarm)
+    {
+        __WFI();
+        backToSleep = 0;
+    }
+
+    // Wake Xbee up
+    XbeeWake();
 
     Disable_RTC();
     //EXTI8Dis();
     clockInit();
     portInit();
     ADCInit();
-		usartInit();    
+    usartInit();
     sysTick_LPF_clkInit();
-		
-		#ifdef DEBUG
-		LCDInit();
-		#endif
+
+#ifdef DEBUG
+    LCDInit();
+#endif
 }
 
 /**
@@ -89,13 +90,13 @@ void EXTI8Dis(void){
 
 void Configure_RTC(uint32_t seconds)
 {
-  /* Enable the peripheral clock RTC */
-  /* (1) Enable the LSI */
-  /* (2) Wait while it is not ready */
-  /* (3) Enable PWR clock */
-  /* (4) Enable write in RTC domain control register */
-  /* (5) LSI for RTC clock */
-  /* (6) Disable PWR clock */
+    /* Enable the peripheral clock RTC */
+    /* (1) Enable the LSI */
+    /* (2) Wait while it is not ready */
+    /* (3) Enable PWR clock */
+    /* (4) Enable write in RTC domain control register */
+    /* (5) LSI for RTC clock */
+    /* (6) Disable PWR clock */
     RCC->CSR |= RCC_CSR_LSION; /* (1) */
     while((RCC->CSR & RCC_CSR_LSIRDY)!=RCC_CSR_LSIRDY) /* (2) */
     {
@@ -106,21 +107,21 @@ void Configure_RTC(uint32_t seconds)
     RCC->BDCR |= RCC_BDCR_BDRST;
     RCC->BDCR &= ~RCC_BDCR_BDRST;
     RCC->BDCR = (RCC->BDCR & ~RCC_BDCR_RTCSEL) | RCC_BDCR_RTCEN | RCC_BDCR_RTCSEL_1; /* (5) */
-    
+
     RCC->APB1ENR &=~ RCC_APB1ENR_PWREN; /* (6) */
-    
-  /* Configure RTC */
-  /* (7) Write access for RTC regsiters */
-  /* (8) Disable wake up timerto modify it */
-  /* (9) Wait until it is allow to modify wake up reload value */
-  /* (10) Modify wake upvalue reload counter to have a wake up each 1Hz */
-  /* (11) Enable wake up counter and wake up interrupt */
-  /* (12) Disable write access */
-    
+
+    /* Configure RTC */
+    /* (7) Write access for RTC regsiters */
+    /* (8) Disable wake up timerto modify it */
+    /* (9) Wait until it is allow to modify wake up reload value */
+    /* (10) Modify wake upvalue reload counter to have a wake up each 1Hz */
+    /* (11) Enable wake up counter and wake up interrupt */
+    /* (12) Disable write access */
+
     while((RTC->CRL & RTC_CRL_RSF) != RTC_CRL_RSF); //wait for synch
     while((RTC->CRL & RTC_CRL_RTOFF) != RTC_CRL_RTOFF); //wait for RTC avail
     RTC->CRL |= RTC_CRL_CNF;
-    RTC->CRH &= ~RTC_CRH_ALRIE; 
+    RTC->CRH &= ~RTC_CRH_ALRIE;
     RTC->CRL &= ~RTC_CRL_CNF;
     while((RTC->CRL & RTC_CRL_RTOFF) != RTC_CRL_RTOFF);
     RTC->CRL |= RTC_CRL_CNF; /* (7) */
@@ -149,7 +150,7 @@ void Configure_RTC(uint32_t seconds)
     EXTI->PR |= EXTI_PR_PR17;
     NVIC->ICPR[1] |= NVIC_ICPR_CLRPEND_9;
     NVIC->ISER[1] |= NVIC_ISER_SETENA_9;
-    
+
 }
 
 void Disable_RTC(void) {
@@ -160,7 +161,7 @@ void Disable_RTC(void) {
     RTC->CRL &=~ RTC_CRL_ALRF;
     while((RTC->CRL & RTC_CRL_RTOFF) != RTC_CRL_RTOFF); //wait for RTC avail
     RTC->CRL |= RTC_CRL_CNF;
-    RTC->CRH &= ~RTC_CRH_ALRIE; 
+    RTC->CRH &= ~RTC_CRH_ALRIE;
     RTC->CRL &= ~RTC_CRL_CNF;
     while((RTC->CRL & RTC_CRL_RTOFF) != RTC_CRL_RTOFF);
 }
